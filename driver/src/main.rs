bd29f2dcaf3828a8f14292c1a9d3b9f80ffdb52c
Martin Geisse <martingeisse@googlemail.com>
2023-07-16 12:18:26 +0200

    ...

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,54 +1,119 @@
-use std::{env, error};
+use std::{env, error, io, time, fs};
+use io::Error;
+use input_linux::sys;
 
-mod joystick;
-mod serial;
+const BAUD_RATE: u32 = 38400;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let args: Vec<_> = env::args().collect();
-    let port = if args.len() > 1 {
-        args[1].clone()
-    } else {
-        "/dev/ttyUSB1".to_owned()
+
+    // create serial port
+    let serial_port_result: Result<Box<dyn serialport::SerialPort>, Error> = {
+        let args: Vec<_> = env::args().collect();
+        let serial_port_device_file_path: String = if args.len() > 1 {
+            args[1].clone()
+        } else {
+            "/dev/ttyUSB1".to_owned()
+        };
+        println!("Connecting to serial port at {}", serial_port_device_file_path);
+        let settings = serialport::SerialPortSettings {
+            baud_rate: BAUD_RATE,
+            data_bits: serialport::DataBits::Eight,
+            flow_control: serialport::FlowControl::None,
+            parity: serialport::Parity::None,
+            stop_bits: serialport::StopBits::One,
+            timeout: time::Duration::from_millis(5000),
+        };
+        Ok(serialport::open_with_settings::<String>(&serial_port_device_file_path.into(), &settings)?)
     };
+    let mut serial_port: Box<dyn serialport::SerialPort> = serial_port_result?;
 
-    println!("Connecting to serial port at {}", port);
-    let mut serial = serial::SerialConnection::new(&port.into(), 38400)?;
+    // create joystick device
+    let joystick_device_result: Result<input_linux::UInputHandle<fs::File>, Error> = {
+        let uinput_file = fs::File::create("/dev/uinput")?;
+        let device = input_linux::UInputHandle::new(uinput_file);
 
-    let joystick = joystick::Joystick::new()?;
+        let input_id = input_linux::InputId {
+            bustype: sys::BUS_VIRTUAL,
+            vendor: 34,
+            product: 10,
+            version: 1,
+        };
 
-    println!(
-        "Created joystick with device path {}",
-        joystick.device_path()?.to_string_lossy()
-    );
+        device.set_evbit(input_linux::EventKind::Absolute)?; // TODO remove?
+        device.set_evbit(input_linux::EventKind::Key)?;
+        device.set_keybit(input_linux::Key::ButtonTrigger)?; // needed -- informs linux that this is a joystick
 
+        device.set_keybit(input_linux::Key::ButtonSouth)?;
+        device.set_keybit(input_linux::Key::ButtonWest)?;
+        device.set_keybit(input_linux::Key::ButtonSelect)?;
+        device.set_keybit(input_linux::Key::ButtonStart)?;
+        device.set_keybit(input_linux::Key::ButtonDpadUp)?;
+        device.set_keybit(input_linux::Key::ButtonDpadDown)?;
+        device.set_keybit(input_linux::Key::ButtonDpadLeft)?;
+        device.set_keybit(input_linux::Key::ButtonDpadRight)?;
+        device.set_keybit(input_linux::Key::ButtonEast)?;
+        device.set_keybit(input_linux::Key::ButtonNorth)?;
+        device.set_keybit(input_linux::Key::ButtonTL)?;
+        device.set_keybit(input_linux::Key::ButtonTR)?;
+
+        device.create(&input_id, b"arduino-snes-pad", 0, &([] as [input_linux::AbsoluteInfoSetup; 0]))?;
+
+        println!("Created joystick with device path {}", device.evdev_path()?.to_string_lossy());
+        Ok(device)
+    };
+    let joystick_device: input_linux::UInputHandle<fs::File> = joystick_device_result?;
+
+    // main loop
+    let mut state: u16 = 0;
     loop {
-        let button_state = serial.read_button_state()?;
-
-        for (i, &pressed) in button_state.pressed.iter().enumerate() {
-            joystick.button_press(button_map(i), pressed)?;
+        let mut buffer: [u8; 1] = [0; 1];
+        if serial_port.read(&mut buffer)? == 0 {
+            continue;
         }
+        state = (state >> 8) + ((buffer[0] as u16) << 8);
+        if (state & 32768) != 0 {
 
-        joystick.synchronise()?;
+            // B, Y, SELECT, START, UP, DOWN, LEFT, RIGHT, A, X, L, R
+
+            handle_button(&joystick_device, state, 0, input_linux::Key::ButtonSouth)?;
+            handle_button(&joystick_device, state, 1, input_linux::Key::ButtonWest)?;
+            handle_button(&joystick_device, state, 2, input_linux::Key::ButtonSelect)?;
+            handle_button(&joystick_device, state, 3, input_linux::Key::ButtonStart)?;
+            handle_button(&joystick_device, state, 4, input_linux::Key::ButtonDpadUp)?;
+            handle_button(&joystick_device, state, 5, input_linux::Key::ButtonDpadDown)?;
+
+            handle_button(&joystick_device, state, 8, input_linux::Key::ButtonDpadLeft)?;
+            handle_button(&joystick_device, state, 9, input_linux::Key::ButtonDpadRight)?;
+            handle_button(&joystick_device, state, 10, input_linux::Key::ButtonEast)?;
+            handle_button(&joystick_device, state, 11, input_linux::Key::ButtonNorth)?;
+            handle_button(&joystick_device, state, 12, input_linux::Key::ButtonTL)?;
+            handle_button(&joystick_device, state, 13, input_linux::Key::ButtonTR)?;
+
+            write_event(&joystick_device, input_linux::SynchronizeEvent::report(empty_event_time()))?;
+
+        }
     }
 }
 
-fn button_map(i: usize) -> joystick::Button {
-    use joystick::Button::*;
-    match i {
-        0 => LeftNorth,
-        1 => LeftWest,
-        2 => LeftEast,
-        3 => LeftSouth,
-        4 => LeftSpecial,
-        5 => RightSouth,
-        6 => RightSpecial,
-        7 => RightEast,
-        8 => RightWest,
-        9 => RightNorth,
-        10 => R2,
-        11 => R1,
-        12 => L2,
-        13 => L1,
-        _ => unreachable!(),
-    }
+fn empty_event_time() -> input_linux::EventTime {
+    input_linux::EventTime::new(0, 0)
+}
+
+fn write_event(
+    joystick_device: &input_linux::UInputHandle<fs::File>,
+    event: impl std::convert::AsRef<sys::input_event>
+) -> Result<(), Error> {
+    joystick_device.write(&[*event.as_ref()])?;
+    Ok(())
+}
+
+fn handle_button(
+    joystick_device: &input_linux::UInputHandle<fs::File>,
+    state: u16,
+    bit: u32,
+    key: input_linux::Key
+) -> Result<(), Error> {
+    let key_state = if ((state >> bit) & 1) == 1 {input_linux::KeyState::PRESSED} else {input_linux::KeyState::RELEASED};
+    write_event(joystick_device, input_linux::KeyEvent::new(empty_event_time(), key, key_state))?;
+    Ok(())
 }
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -43,12 +43,29 @@ void loop() {
   myDelay(1);
   for (int i = 0; i < 16; i++) {
     buttonState[i] = digitalRead(5) == LOW;
-    Serial.print(buttonState[i] ? "#" : "-");
     digitalWrite(7, LOW);
     myDelay(1);
     digitalWrite(7, HIGH);
     myDelay(1);
   }
-  Serial.println();
+  Serial.write(
+    (buttonState[0] ? 1 : 0) +
+    (buttonState[1] ? 2 : 0) +
+    (buttonState[2] ? 4 : 0) +
+    (buttonState[3] ? 8 : 0) +
+    (buttonState[4] ? 16 : 0) +
+    (buttonState[5] ? 32 : 0)
+  );
+  Serial.write(
+    (buttonState[6] ? 1 : 0) +
+    (buttonState[7] ? 2 : 0) +
+    (buttonState[8] ? 4 : 0) +
+    (buttonState[9] ? 8 : 0) +
+    (buttonState[10] ? 16 : 0) +
+    (buttonState[11] ? 32 : 0) +
+    128
+  );
   myDelay(10);
+
+  myDelay(500);
 }
